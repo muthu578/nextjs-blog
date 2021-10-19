6972df91f11f4257d6f9ba09a95a7531b7416b80
muthu578 <muthummk92@gmail.com>
2021-10-19 12:23:34 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,7 @@ export default function Home({ allPostsData }) {
       {/* Keep the existing code here */}
 
       {/* Add this <section> tag below the existing <section> tag */}
+      <p> Welcome to Tutorial</p>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
